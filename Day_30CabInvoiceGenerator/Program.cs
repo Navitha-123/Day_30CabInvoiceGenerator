2d285f2a7a159ded02fcd20c4dccbdbc9ea3834a
navitha <navithareddy112@gmail.com>
2022-10-06 20:25:24 +0530

    [navitha] Day_30CabInvoiceGenerator

---
+++ Day_30CabInvoiceGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Day_30CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double Fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare for Normal ride is " +Fare);
+
+
+            InvoiceGenerator invoiceGenerator1 = new InvoiceGenerator(RideType.PREMIUM);
+            double Fare1 = invoiceGenerator.CalculateFare(2.0, 20);
+            Console.WriteLine("Total fare for Normal ride is " + Fare1);
+
+
+
+
+        }
+    }
+}
